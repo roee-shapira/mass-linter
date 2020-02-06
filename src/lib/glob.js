b28f2268f5e354cc0af3e39f8b1cedb57746daa3
Roee Shapira <roee.shapira@gong.io>
2020-02-06 09:39:25 +0200

    Minor major edit.

--- src/lib/glob.js
+++ src/lib/glob.js
@@ -2,7 +2,7 @@ const globCallback = require('glob');
 
 module.exports = {
 	glob(url, options) {
-		new Promise((resolve, reject) =>
+		return new Promise((resolve, reject) =>
 			globCallback(url, options, (err, res) => {
 				if (err) return reject(err);
 				return resolve(res);
